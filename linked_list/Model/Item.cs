7a214d2ef0bc0f715721f848b036fe39b71fd9d3
Хисматулина Карина <hkarinaf@gmail.com>
2020-03-10 11:53:34 +0300

    Add project files.

---
+++ linked_list/Model/Item.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace linked_list.Model
+{
+    public class Item<T>
+    {
+        private T data = default(T);
+        //private Item<T> next = null;
+
+        public T Data
+        {
+            get => data; //?
+            set
+            {
+                if (data != null) data = value;
+                else throw new ArgumentNullException(nameof(value));
+            }
+        }
+
+        public Item<T> Next { get; set;}
+
+        public Item(T Data)
+        {
+            Data = data;
+        }
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
---
+++ linked_list/Model/LinkedList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace linked_list.Model
+{
+    public class LinkedList<T>
+    {
+        public Item<T> Head { get; private set; }
+        public Item<T> Tail { get; private set }
+        public int count { get; private set; }
+
+        public LinkedList()
+        {
+            Head = null;
+            Tail = null;
+            count = 0;
+        }
+
+        public LinkedList(T Data)
+        {
+            Item<T> item = new Item<T>(Data); // or var itm = new Item(Data);
+            Head = item;
+            Tail = item;
+            count = 1;
+        }
+
+        public void Add(T data)
+        {
+            Item <T> item = new Item<T>(data);
+            if (Tail != null)
+            {
+                Tail.Next = item;
+                Tail = item;
+            }
+        }
+    }
+}
